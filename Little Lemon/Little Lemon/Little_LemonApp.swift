1837dc25a217f780759afc1aa125a4d76d10dfd1
hemakatakam <hkatakam@syr.edu>
2025-01-12 16:58:24 -0500

    iOS app code started

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Nagababu Katakam on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
